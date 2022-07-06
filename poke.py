4c7ec675927b41e7ccd980dfcfb233fc29fda4c2
Mattias Fransson <mattias@webben.nu>
2022-07-06 14:27:19 +0200

    look up if mac address

--- poke.py
+++ poke.py
@@ -1,1 +1,7 @@
 #!/usr/local/bin/python3
+#https://redirect.teliawifi.telia.com/portal?mac=be:ef:be:ef:be:ef
+import psutil
+psutil.net_if_addrs()
+network_interface='iw0'
+mac_address=list(psutil.net_if_addrs()[network_interface][2])[1]
+print(f"if {network_interface} mac: {mac_address}")
