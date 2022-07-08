cefb3ad9ca0ba91a59797b2d3aafe039f56d6737
Mattias Fransson <mattias@webben.nu>
2022-07-08 13:42:01 +0200

    post e-mail to session

--- poke.py
+++ poke.py
@@ -2,6 +2,7 @@
 #https://redirect.teliawifi.telia.com/portal?mac=be:ef:be:ef:be:ef
 import psutil
 import requests
+post_json = { "email": "user@email.com" }
 psutil.net_if_addrs()
 network_interface='iw0'
 mac_address=list(psutil.net_if_addrs()[network_interface][2])[1]
@@ -15,3 +16,9 @@ for varstring in response.url.split('?')[1].split('&'):
   namestr, value = varstring.split('=')
   exec("%s = '%s'" % (namestr, value))
 print(f"session_token: {session_token}, traceparent: {traceparent}")
+post_response=requests.post(f'https://cp.teliawifi.telia.com/TW-Reg/api/telia/v1/email_registration/{session_token}', json = post_json)
+
+if post_response.status_code == 204:
+  print(f"Succesful post of e-mail address!")
+else:
+  print(f"unknown error, post http status code: '{post_response.status_code}")
